a2705a161eeb947254dc1ddb4b31c2ed07d18ad8
Aviral Prakash <aviralprakash@gmail.com>
2014-09-30 21:07:22 +0800

    update: linkedlist class of type task
    functions: add task, delete by task, delete by index, size, get task by
    index,.

--- LifeOnLine/src/lol/TaskList.java
+++ LifeOnLine/src/lol/TaskList.java
@@ -1,5 +1,41 @@
 package lol;
 
+import java.util.LinkedList;
+
 public class TaskList {
 
+	private static LinkedList<Task> list = new LinkedList<Task>();
+
+	public static boolean add(Task taskObject) {
+		return list.add(taskObject);
+	}
+
+	public static boolean delete(Task taskObject) {
+		return list.removeFirstOccurrence(taskObject);
+	}
+
+	public static boolean deleteByIndex(int index) {
+		try {
+			list.remove(index);
+		} catch (IndexOutOfBoundsException e) {
+			return false;
+		}
+		return true;
+	}
+
+	public static LinkedList<Task> getList() {
+		return list;
+	}
+	
+	public static int size(){
+		return list.size();
+	}
+	
+	public static Task get(int index){
+		try {
+			return list.get(index);
+		} catch (IndexOutOfBoundsException e) {
+			return null;
+		}
+	}
 }
