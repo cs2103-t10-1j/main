59ea04426dfa7ec5e545dcb84cf48320dafafd00
Rama <nrama92@gmail.com>
2014-10-01 18:29:37 +0800

    Updated LOLControl to return feedback in String format

--- LifeOnLine/src/lol/LOLControl.java
+++ LifeOnLine/src/lol/LOLControl.java
@@ -1,87 +1,68 @@
 package lol;
 
-import java.LinkedList;
-
 public class LOLControl {
 
-  public static final String COMMAND_ADD = "add";
-  public static final String COMMAND_DELETE = "delete";
-  public static final String COMMAND_INVALID = "invalid command";
-  public static final String FEEDBACK_ADD_SUCCESS = " added successfully";
-  public static final String FEEDBACK_DEL_SUCCESS = " deleted successfully";
-  public static final String FEEDBACK_INVALID = " is an invalid action.";
+	public static final String QUOTE = "\"";
+	public static final String COMMAND_ADD = "add";
+	public static final String COMMAND_DEL = "delete";
+	public static final String COMMAND_INVALID = "invalid command";
+	public static final String FEEDBACK_ADD_SUCCESS = "added successfully";
+	public static final String FEEDBACK_DEL_SUCCESS = "deleted successfully";
+	public static final String FEEDBACK_INVALID = "That is an invalid action";
 
-  /********** Load Storage ***********/
+	/********** Load Storage ***********/
 
-  LinkedList<Task> list = storage.load();
+	public static TaskList list = LOLStorage.load();
 
+	/********** Controller methods ***********/
 
-  /********** Controller methods ***********/
+	public static String executeUserInput(String userInput) {
+		if (getCommandType(userInput) == "COMMAND_ADD") {
+			return executeAdd(userInput);
+		}
 
-  public static void executeUserInput(String userInput) {
-    if (getCommandType(userInput) == "COMMAND_ADD") {
-      executeAdd(userInput);
-    }
+		if (getCommandType(userInput) == "COMMAND_DEL") {
+			return executeDel(userInput);
+		}
+		if (getCommandType(userInput) == "COMMAND_INVALID") {
+			return executeInvalid(userInput);
+		}
+	}
 
-    if (getCommandType(userInput) == "COMMAND_DEL") {
-      executeDel(userInput);
+	private static String getCommandType(String userInput) {
+		String command = LOLParser.getCommandName(userInput);
+		return command;
+	}
 
-      if (getCommandType(userInput) == "COMMAND_INVALID")
-      executeInvalid(userInput);
-    }
-  }
+	private static String executeAdd(String userInput) {
+		Task newTask = LOLParser.getTask(userInput);
+		list.add(newTask);
+		LOLStorage.save();
+		return showFeedback(newTask, COMMAND_ADD);
+	}
 
-  private static String getCommandType(String userInput) {
-    String command = LOLParser.getCommandName(userInput);
-    return command;
-  }
+	private static String executeDel(String userInput) {
+		Task delTask = LOLParser.getTask(userInput);
+		list.delete(delTask);
+		LOLStorage.save();
+		return showFeedback(delTask, COMMAND_DEL);
+	}
 
-  private static void executeAdd(String userInput) {
-    Task newTask = LOLParser.getTask(userInput);
-    list.add(newTask);
-    storage.save();
-    showFeedback(newTask, COMMAND_ADD);
-  }
+	private static String executeInvalid(String userInput) {
+		Task invalidTask = LOLParser.getTask(userInput);
+		return showFeedback(invalidTask, COMMAND_INVALID);
+	}
 
-  private static void executeDel(String userInput) {
-    Task delTask = LOLParser.getTask(userInput);
-    list.delete(delTask);
-    storage.save();
-    showFeedback(delTask, COMMAND_DEL);
-  }
-
-  private static void executeInvalid(String userInput) {
-    Task invalidTask = LOL.Parser.getTask(userInput);
-    showFeedback(invalidTask, COMMAND_INVALID);
-  }
-
-  private static void showFeedback(Task task, String commandType) {
-    if (commandType == COMMAND_ADD) {
-      System.out.println(+ Task + FEEDBACK_ADD_SUCCESS);
-    }
-    if (commandType == COMMAND_DEL) {
-      System.out.println(+ Task + FEEDBACK_DEL_SUCCESS);
-    }
-    if (commandType == COMMAND_INVALID) {
-      System.out.println(+ Task + FEEDBACK_INVALID);
-    }
-  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	private static String showFeedback(Task task, String commandType) {
+		if (commandType == COMMAND_ADD) {
+			return (QUOTE + task + QUOTE + FEEDBACK_ADD_SUCCESS);
+		}
+		if (commandType == COMMAND_DEL) {
+			return (QUOTE + task + QUOTE + FEEDBACK_DEL_SUCCESS);
+		}
+		if (commandType == COMMAND_INVALID) {
+			return (FEEDBACK_INVALID);
+		}
+	}
 
 }
