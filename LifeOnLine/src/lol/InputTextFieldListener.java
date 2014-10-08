0599ce875a3910c5bc22eb2343a5f47d6d467772
sevin <liliyun940216@gmail.com>
2014-10-08 17:32:19 +0800

    can display start time and end time in display panel

--- LifeOnLine/src/lol/InputTextFieldListener.java
+++ LifeOnLine/src/lol/InputTextFieldListener.java
@@ -44,25 +44,38 @@ public class InputTextFieldListener implements ActionListener {
 
 		for(int i = 0; i < taskList.size(); i++){
 			Task task = taskList.get(i);
-			String taskDescription = task.getTaskDescription();
-			
+			String taskDescription = task.getTaskDescription(); //useless line? refer line 62
+			Time dueStartTime = task.getStartTime();
+			Time dueEndTime = task.getEndTime();
+
 			Date currentDueDate = task.getTaskDueDate();
 			if(currentDueDate != null && !currentDueDate.equals(previousDueDate)){
 				int dueDay = currentDueDate.getDay();
 				int dueMonth = currentDueDate.getMonth();
 				int dueYear = currentDueDate.getYear4Digit();
-				
+
 				strToShow = strToShow + dateFormatAsHeader(dueDay, dueMonth, dueYear);
 			}
 			else if(currentDueDate == null && previousDueDate != null){
 				strToShow = strToShow + dateFormatAsHeader(0, 0, 0);
 			}
 			previousDueDate = currentDueDate;
-			
-			strToShow = strToShow + formatString(taskDescription, i+1);
+
+			String str = taskDescription;
+			if(dueStartTime != null && dueEndTime != null){
+				str = addTimeStr(str, dueStartTime, dueEndTime);
+			}
+			str = formatString(str, i+1);
+
+			strToShow = strToShow + str;
 		}
 
 		mainDisplayTA.setText(strToShow);
+	}
+
+	public String addTimeStr(String str, Time startTime, Time endTime){
+		str = "[" + startTime.getFormat24hr() + " - " + endTime.getFormat24hr() + "] " + str;
+		return str;
 	}
 
 	public String formatString(String str, int i){
@@ -75,13 +88,13 @@ public class InputTextFieldListener implements ActionListener {
 	public String addNumbering(String str, int number){
 		return number + ". " + str;
 	}
-	
+
 	public String dateFormatAsHeader(int dueDay, int dueMonth, int dueYear){
 		String str = "";
 
 		str = str + "===========================";
 		str = addNewLine(str);
-		
+
 		if(dueDay != 0 && dueMonth != 0 && dueYear != 0){
 			str = str + dueDay + "/" + dueMonth + "/" + dueYear;
 			str = addNewLine(str);	
@@ -90,10 +103,10 @@ public class InputTextFieldListener implements ActionListener {
 			str = str + "To-Do Tasks (without due date)";
 			str = addNewLine(str);
 		}
-		
+
 		str = str + "===========================";
 		str = addNewLine(str);
-		
+
 		return str;
 	}
 
