af1df9d85fd20be3fd12f7aed7e7910373d84d50
sevin <liliyun940216@gmail.com>
2014-11-05 21:01:50 +0800

    can now display multiple day tasks

--- LifeOnLine/src/lol/Date.java
+++ LifeOnLine/src/lol/Date.java
@@ -180,6 +180,23 @@ public class Date {
 			return false;
 		}
 	}
+	
+	public static boolean equalDate(Date date1, Date date2){
+		if(date1 == null && date2 != null){
+			return false;
+		}
+		else if(date1 != null && date2 == null){
+			return false;
+		}
+		else if(date1 == null && date2 == null){
+			return true;
+		}
+		else{ 
+			return date1.getDay() == date2.getDay()
+					&& date1.getMonth() == date2.getMonth()
+					&& date1.getYear4Digit() == date2.getYear4Digit();
+		}
+	}
 
 	/*********** Comparison methods ***********/
 
